15aa9f401234ab3b26d17dbfb8504fae5ff74a63
Matthew ONGARO <ongaro207@gmail.com>
2020-04-29 20:57:19 -0400

    add student updated

--- Lakehead ERIMS/addUpdateStudent.cs
+++ Lakehead ERIMS/addUpdateStudent.cs
@@ -45,23 +45,13 @@ namespace Lakehead_ERIMS
 
         private void addStudentbttn_Click(object sender, EventArgs e)
         {
-            string connenctionString;
-            OleDbConnection cnn;
-            connenctionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\ongar\OneDrive\Desktop\LUEquipment.mdb;Persist Security Info=True";
-            cnn = new OleDbConnection(connenctionString);
-            cnn.Open();
 
-            OleDbCommand cmd = new OleDbCommand();
-
-            string command = "insert into tblStudent(Stu_Number, Stu_LName, Stu_FName, Stu_LAddress,Stu_LCity, Stu_LProvince, Stu_LPCode, Stu_LPhone, Stu_LExt, Stu_Email) values('" + newNumberTextBox.Text + "','" + newLastTextBox.Text + "', '"+ newFirstTextBox.Text + "','"+ newAddressTextBox.Text + "', '" + newCityTextBox.Text + "' ,'" + provComboBox.Text + "','" + newPostalCodeTextBox.Text + "' ,'" + NewPhoneTextBox.Text + "','" + newExtTextBox.Text + "','" + newEmailTextBox.Text + "')";
-          
-            OleDbCommand cmdd = new OleDbCommand(command, cnn);
        
-            cmdd.ExecuteNonQuery();
-        
-            MessageBox.Show("YAYA");
+            LUEquipmentDataSetTableAdapters.tblStudentTableAdapter studentTableAdapter = new LUEquipmentDataSetTableAdapters.tblStudentTableAdapter();
+            studentTableAdapter.Insert(newNumberTextBox.Text,newLastTextBox.Text,newFirstTextBox.Text,newAddressTextBox.Text,newCityTextBox.Text,provComboBox.Text,newPostalCodeTextBox.Text,NewPhoneTextBox.Text,newExtTextBox.Text,newEmailTextBox.Text, " ", " ", " ", " "," "," ",0,true);
+            MessageBox.Show("Added student");
+            
 
-            cnn.Close();
         }
 
         private void exitBttn_Click(object sender, EventArgs e)
@@ -74,8 +64,7 @@ namespace Lakehead_ERIMS
 
             this.tblStudentTableAdapter1.Fill(this.luEquipmentDataSet1.tblStudent);
             DataRow info;
-            MessageBox.Show(studentNumberTextBox.Text);
-            info = luEquipmentDataSet1.tblStudent.Select("Stu_Number = '" + studentNumberTextBox.Text + "'")[1];
+            info = luEquipmentDataSet1.tblStudent.Select("Stu_Number = '" + studentNumberTextBox.Text + "'")[0];
             lastNameInfoTextBox.Text = info[2].ToString();
             fristNameInfoTextBox.Text = info[3].ToString();
             numberInfoTextBox.Text = info[1].ToString();
@@ -87,73 +76,23 @@ namespace Lakehead_ERIMS
             phoneInfoTextBox.Text = info[8].ToString();
             extInfoTextBox.Text = info[9].ToString();
             feeTextBox.Text = info[17].ToString();
-            /*
-            string connenctionString;
-            OleDbConnection cnn;
-            connenctionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\ongar\OneDrive\Desktop\LUEquipment.mdb;Persist Security Info=True";
-            cnn = new OleDbConnection(connenctionString);
-            cnn.Open();
-            OleDbCommand command = new OleDbCommand();
-            command.Connection = cnn;
-            command.CommandText = "SELECT Stu_LName FROM tblStudent WHERE Stu_Number = '" + studentNumberTextBox.Text + "'";
-            lastNameInfoTextBox.Text = command.ExecuteScalar().ToString();
-
-            OleDbCommand command1 = new OleDbCommand();
-            command.Connection = cnn;
-            command.CommandText = "SELECT Stu_FName FROM tblStudent WHERE Stu_Number = '" + studentNumberTextBox.Text + "'";
-            fristNameInfoTextBox.Text = command.ExecuteScalar().ToString();
-
-            OleDbCommand command2 = new OleDbCommand();
-            command.Connection = cnn;
-            command.CommandText = "SELECT Stu_LAddress FROM tblStudent WHERE Stu_Number = '" + studentNumberTextBox.Text + "'";
-            addressInfoTextBox.Text = command.ExecuteScalar().ToString();
-
-            OleDbCommand command3 = new OleDbCommand();
-            command3.Connection = cnn;
-            command3.CommandText = "SELECT Stu_Number FROM tblStudent WHERE Stu_Number = '" + studentNumberTextBox.Text + "'";
-            numberInfoTextBox.Text = command3.ExecuteScalar().ToString();
-
-            OleDbCommand command4 = new OleDbCommand();
-            command4.Connection = cnn;
-            command4.CommandText = "SELECT Stu_Email FROM tblStudent WHERE Stu_Number = '" + studentNumberTextBox.Text + "'";
-            emailInfoTextBox.Text = command4.ExecuteScalar().ToString();
-
-            OleDbCommand command5 = new OleDbCommand();
-            command5.Connection = cnn;
-            command5.CommandText = "SELECT Stu_LCity FROM tblStudent WHERE Stu_Number = '" + studentNumberTextBox.Text + "'";
-            cityInfoTextBox.Text = command5.ExecuteScalar().ToString();
-
-            OleDbCommand command6 = new OleDbCommand();
-            command6.Connection = cnn;
-            command6.CommandText = "SELECT Stu_LProvince FROM tblStudent WHERE Stu_Number = '" + studentNumberTextBox.Text + "'";
-            provInfoCombo.Text = command6.ExecuteScalar().ToString();
-
-            OleDbCommand command7 = new OleDbCommand();
-            command7.Connection = cnn;
-            command7.CommandText = "SELECT Stu_LPCode FROM tblStudent WHERE Stu_Number = '" + studentNumberTextBox.Text + "'";
-            postalInfoText.Text = command7.ExecuteScalar().ToString();
-
-            OleDbCommand command8 = new OleDbCommand();
-            command8.Connection = cnn;
-            command8.CommandText = "SELECT Stu_LPhone FROM tblStudent WHERE Stu_Number = '" + studentNumberTextBox.Text + "'";
-            phoneInfoTextBox.Text = command8.ExecuteScalar().ToString();
-
-            OleDbCommand command9 = new OleDbCommand();
-            command9.Connection = cnn;
-            command9.CommandText = "SELECT Stu_LExt FROM tblStudent WHERE Stu_Number = '" + studentNumberTextBox.Text + "'";
-            extInfoTextBox.Text = command9.ExecuteScalar().ToString();
-
-            OleDbCommand command10 = new OleDbCommand();
-            command10.Connection = cnn;
-            command10.CommandText = "SELECT Stu_Owe FROM tblStudent WHERE Stu_Number = '" + studentNumberTextBox.Text + "'";
-            feeTextBox.Text = command10.ExecuteScalar().ToString();
-            cnn.Close();
-            */
+       
         }
 
         private void updateStudentBttn_Click(object sender, EventArgs e)
         {
+            this.tblStudentTableAdapter1.Fill(this.luEquipmentDataSet1.tblStudent);
+            DataRow id;
+            id = luEquipmentDataSet1.tblStudent.Select("Stu_Number = '" + studentNumberTextBox.Text + "'")[0];
+            string studentId = id[0].ToString();
+            int intStudentId = Int32.Parse(studentId);
+            float fee = (float)Convert.ToDouble(feeTextBox.Text);
 
+
+            LUEquipmentDataSetTableAdapters.tblStudentTableAdapter tblStudentTableAdapter = new LUEquipmentDataSetTableAdapters.tblStudentTableAdapter();
+            tblStudentTableAdapter.Update(numberInfoTextBox.Text, lastNameInfoTextBox.Text, fristNameInfoTextBox.Text, addressInfoTextBox.Text, cityInfoTextBox.Text, provInfoCombo.Text, postalInfoText.Text, phoneInfoTextBox.Text, extInfoTextBox.Text, emailInfoTextBox.Text, "", "", "", "", "", "", fee, true, intStudentId, studentNumberTextBox.Text, lastNameInfoTextBox.Text, fristNameInfoTextBox.Text, addressInfoTextBox.Text, cityInfoTextBox.Text, provInfoCombo.Text, postalInfoText.Text, phoneInfoTextBox.Text, extInfoTextBox.Text, emailInfoTextBox.Text, "", "", "", "", "", "", fee, true);
+            MessageBox.Show("Updated");
+            //LUEquipmentDataSetTableAdapters.tblStudentTableAdapter
             /*
             string connenctionString;
             OleDbConnection cnn;
