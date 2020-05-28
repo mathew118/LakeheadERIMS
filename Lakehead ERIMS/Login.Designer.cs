72ec030f71eb7fdd1d8347185be786ebf6d10672
Mathew Sousa <mathew1181@hotmail.com>
2020-05-27 22:27:36 -0400

    Disabled login skip button

--- Lakehead ERIMS/Login.Designer.cs
+++ Lakehead ERIMS/Login.Designer.cs
@@ -119,12 +119,14 @@
             // button1
             // 
             this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Enabled = false;
             this.button1.Location = new System.Drawing.Point(111, 163);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 15;
             this.button1.Text = "Skip";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Login
